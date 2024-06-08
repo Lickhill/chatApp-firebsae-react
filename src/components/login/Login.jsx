51b233a104256996f18a2e72a9ad292d084f3f12
Likhil N Maiya <134200383+Lickhill@users.noreply.github.com>
2024-06-08 12:55:34 +0000

    made some changse

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify"; // Import toast from react-toastify
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from "firebase/auth";
 // In Login.jsx
 import { auth, db } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
@@ -71,6 +74,17 @@ const Login = () => {
 
 	const handelLogin = async (e) => {
 		e.preventDefault();
+		setloading(true);
+		const formData = new FormData(e.target);
+		const { email, password } = Object.fromEntries(formData);
+		try {
+			await signInWithEmailAndPassword(auth, email, password);
+		} catch (err) {
+			console.log(err);
+			toast.error(err.message);
+		} finally {
+			setloading(false);
+		}
 	};
 
 	return (
@@ -110,7 +124,7 @@ const Login = () => {
 						placeholder="Password"
 					/>
 					<button disabled={loading} className="button">
-						{loading ? "Loading..." : "Sign in"}
+						{loading ? "Loading..." : "Sign up"}
 					</button>
 				</form>
 			</div>
