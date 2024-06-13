e6f7d009b5abb116313b29e5f690853ac83dc355
Likhil N Maiya <134200383+Lickhill@users.noreply.github.com>
2024-06-13 12:48:59 +0000

    made some changes

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -2,25 +2,25 @@ import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "./firebase";
 
 const upload = async (file) => {
-	const storageRef = ref(storage, "images/rivers.jpg");
+	const date = new Date();
+	const storageRef = ref(storage, `images/${date + file.name}`);
 	const uploadTask = uploadBytesResumable(storageRef, file);
 
 	return new Promise((resolve, reject) => {
-		const date = new Date();
 		uploadTask.on(
 			"state_changed",
 			(snapshot) => {
 				const progress =
 					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 				console.log("Upload is " + progress + "% done");
-				switch (snapshot.state) {
-					case "paused":
-						console.log("Upload is paused");
-						break;
-					case "running":
-						console.log("Upload is running");
-						break;
-				}
+				// switch (snapshot.state) {
+				// 	case "paused":
+				// 		console.log("Upload is paused");
+				// 		break;
+				// 	case "running":
+				// 		console.log("Upload is running");
+				// 		break;
+				// }
 			},
 			(error) => {
 				reject("Something went wrong " + error.code);
