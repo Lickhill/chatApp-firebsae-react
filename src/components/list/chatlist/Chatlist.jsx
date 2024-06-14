1672ef37f47303e2bab9140cf5113c4c42d275ba
Likhil N Maiya <134200383+Lickhill@users.noreply.github.com>
2024-06-14 07:47:01 +0000

    made some changes

--- src/components/list/chatlist/Chatlist.jsx
+++ src/components/list/chatlist/Chatlist.jsx
@@ -1,9 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./chatlist.css";
 import AddUser from "./adduser/AddUser";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
+import { useStore } from "zustand";
+import { useUserStore } from "../../../lib/userStore";
 
 const Chatlist = () => {
 	const [addMode, setaddMode] = useState(false);
+	const [chats, setChats] = useState([]);
+	const { currentUser } = useUserStore();
+
+	useEffect(() => {
+		const unsub = onSnapshot(
+			doc(db, "userchats", currentUser.id),
+			async (res) => {
+				const items = res.data().chats;
+				const promises = items.map(async (item) => {
+					const userDocRef = doc(db, "users", item.receiverId);
+					const userDocSnap = await getDoc(userDocRef);
+
+					const user = userDocSnap.data();
+
+					return { ...item, user };
+				});
+				const chatData = await Promise.all(promises);
+
+				setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+			}
+		);
+
+		return () => {
+			unsub();
+		};
+	}, [currentUser]);
+
 	return (
 		<div className="chatList">
 			<div className="search">
@@ -19,34 +50,16 @@ const Chatlist = () => {
 				/>
 			</div>
 
-			<div className="item">
-				<img src="./avatar.png" alt="person's dp" />
-				<div className="texts">
-					<span>Daaeyum</span>
-					<p>Hello</p>
+			{chats.map((chat) => (
+				<div className="item" key={chat.chatId}>
+					<img src="./avatar.png" alt="person's dp" />
+					<div className="texts">
+						<span>Daaeyum</span>
+						<p>{chat.lastMessage}</p>
+					</div>
 				</div>
-			</div>
-			<div className="item">
-				<img src="./avatar.png" alt="person's dp" />
-				<div className="texts">
-					<span>Daaeyum</span>
-					<p>Hello</p>
-				</div>
-			</div>
-			<div className="item">
-				<img src="./avatar.png" alt="person's dp" />
-				<div className="texts">
-					<span>Daaeyum</span>
-					<p>Hello</p>
-				</div>
-			</div>
-			<div className="item">
-				<img src="./avatar.png" alt="person's dp" />
-				<div className="texts">
-					<span>Daaeyum</span>
-					<p>Hello</p>
-				</div>
-			</div>
+			))}
+
 			{addMode ? <AddUser /> : null}
 		</div>
 	);
